7c31b7d00906a1f1ee6b337dcd5b559ba5660f9d
ASHISH KUMAR YADAV <ashish_business@yahoo.com>
2020-11-22 11:42:11 +0530

    react added

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Serve static files from the React app
-// app.use(express.static(path.join(__dirname, 'client/build')));
+//Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/',(req,res)=>{
-    res.send("<h1>hii this is express</h1>");
-});
+// app.get('/',(req,res)=>{
+//     res.send("<h1>hii this is express</h1>");
+// });
 
 
 
@@ -23,9 +23,9 @@ app.get('/',(req,res)=>{
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
